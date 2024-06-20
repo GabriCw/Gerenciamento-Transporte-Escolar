955adf27ed638952c2c9fcbb914ff0f288918673
felipe-silvieri-braslive <felipesilvieri@braslive.com>
2024-06-19 23:25:54 -0300

    scrollview_pt1

--- AppTransporteEscolar/View/RegisterMotorista.js
+++ AppTransporteEscolar/View/RegisterMotorista.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 
 const RegisterMotoristaScreen = ({ navigation }) => {
@@ -18,83 +18,88 @@ const RegisterMotoristaScreen = ({ navigation }) => {
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Button 
-                    mode="contained" 
-                    onPress={() => navigation.navigate('Register')} 
-                    style={styles.button}
-                    labelStyle={styles.buttonLabel}
-                >
-                    Voltar
-                </Button>
-            </View>
-            <View style={styles.content}>
-                <Text style={styles.title}>Cadastro do Motorista</Text>
-                <TextInput
-                    style={styles.input}
-                    label="Nome"
-                    mode="outlined"
-                    value={nome}
-                    onChangeText={text => setNome(text)}
-                />
-                <TextInput
-                    style={styles.input}
-                    label="CPF"
-                    mode="outlined"
-                    value={cpf}
-                    onChangeText={text => setCpf(text)}
-                    keyboardType="numeric"
-                />
-                <TextInput
-                    style={styles.input}
-                    label="Telefone"
-                    mode="outlined"
-                    value={telefone}
-                    onChangeText={text => setTelefone(text)}
-                    keyboardType="phone-pad"
-                />
-                <TextInput
-                    style={styles.input}
-                    label="Placa do Veículo"
-                    mode="outlined"
-                    value={placa}
-                    onChangeText={text => setPlaca(text)}
-                />
-                <TextInput
-                    style={styles.input}
-                    label="E-mail"
-                    mode="outlined"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    keyboardType="email-address"
-                />
-                <TextInput
-                    style={styles.input}
-                    label="Senha"
-                    mode="outlined"
-                    value={senha}
-                    onChangeText={text => setSenha(text)}
-                    secureTextEntry={true}
-                />
-                <TextInput
-                    style={styles.input}
-                    label="Confirme sua Senha"
-                    mode="outlined"
-                    value={confSenha}
-                    onChangeText={text => setConfSenha(text)}
-                    secureTextEntry={true}
-                />
-                <Button 
-                    title="Cadastrar" 
-                    onPress={handleCadastro} 
-                    style={styles.button}
-                    labelStyle={styles.buttonLabel}
-                >
-                    Cadastrar
-                </Button>
-            </View>
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 2 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <ScrollView contentContainerStyle={styles.container}>
+                <View style={styles.header}>
+                    <Button 
+                        mode="contained" 
+                        onPress={() => navigation.navigate('Register')} 
+                        style={styles.button}
+                        labelStyle={styles.buttonLabel}
+                    >
+                        Voltar
+                    </Button>
+                </View>
+                <View style={styles.content}>
+                    <Text style={styles.title}>Cadastro do Motorista</Text>
+                    <TextInput
+                        style={styles.input}
+                        label="Nome"
+                        mode="outlined"
+                        value={nome}
+                        onChangeText={text => setNome(text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="CPF"
+                        mode="outlined"
+                        value={cpf}
+                        onChangeText={text => setCpf(text)}
+                        keyboardType="numeric"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="Telefone"
+                        mode="outlined"
+                        value={telefone}
+                        onChangeText={text => setTelefone(text)}
+                        keyboardType="phone-pad"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="Placa do Veículo"
+                        mode="outlined"
+                        value={placa}
+                        onChangeText={text => setPlaca(text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="E-mail"
+                        mode="outlined"
+                        value={email}
+                        onChangeText={text => setEmail(text)}
+                        keyboardType="email-address"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="Senha"
+                        mode="outlined"
+                        value={senha}
+                        onChangeText={text => setSenha(text)}
+                        secureTextEntry={true}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="Confirme sua Senha"
+                        mode="outlined"
+                        value={confSenha}
+                        onChangeText={text => setConfSenha(text)}
+                        secureTextEntry={true}
+                    />
+                    <Button 
+                        title="Cadastrar" 
+                        onPress={handleCadastro} 
+                        style={styles.button}
+                        labelStyle={styles.buttonLabel}
+                    >
+                        Cadastrar
+                    </Button>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
--- AppTransporteEscolar/View/RegisterResponsavel.js
+++ AppTransporteEscolar/View/RegisterResponsavel.js
@@ -17,76 +17,81 @@ const RegisterResponsavelScreen = ({ navigation }) => {
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Button 
-                    mode="contained" 
-                    onPress={() => navigation.navigate('Register')} 
-                    style={styles.button}
-                    labelStyle={styles.buttonLabel}
-                >
-                    Voltar
-                </Button>
-            </View>
-            <View style={styles.content}>
-                <Text style={styles.title}>Cadastro do Responsável</Text>
-                <TextInput
-                    style={styles.input}
-                    label="Nome"
-                    mode="outlined"
-                    value={nome}
-                    onChangeText={text => setNome(text)}
-                />
-                <TextInput
-                    style={styles.input}
-                    label="CPF"
-                    mode="outlined"
-                    value={cpf}
-                    onChangeText={text => setCpf(text)}
-                    keyboardType="numeric"
-                />
-                <TextInput
-                    style={styles.input}
-                    label="Telefone"
-                    mode="outlined"
-                    value={telefone}
-                    onChangeText={text => setTelefone(text)}
-                    keyboardType="phone-pad"
-                />
-                <TextInput
-                    style={styles.input}
-                    label="E-mail"
-                    mode="outlined"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    keyboardType="email-address"
-                />
-                <TextInput
-                    style={styles.input}
-                    label="Senha"
-                    mode="outlined"
-                    value={senha}
-                    onChangeText={text => setSenha(text)}
-                    secureTextEntry={true}
-                />
-                <TextInput
-                    style={styles.input}
-                    label="Confirme sua Senha"
-                    mode="outlined"
-                    value={confSenha}
-                    onChangeText={text => setConfSenha(text)}
-                    secureTextEntry={true}
-                />
-                <Button 
-                    title="Cadastrar" 
-                    onPress={handleCadastro} 
-                    style={styles.button}
-                    labelStyle={styles.buttonLabel}
-                >
-                    Cadastrar
-                </Button>
-            </View>
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 2 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+            <ScrollView contentContainerStyle={styles.container}>
+                <View style={styles.header}>
+                    <Button 
+                        mode="contained" 
+                        onPress={() => navigation.navigate('Register')} 
+                        style={styles.button}
+                        labelStyle={styles.buttonLabel}
+                    >
+                        Voltar
+                    </Button>
+                </View>
+                <View style={styles.content}>
+                    <Text style={styles.title}>Cadastro do Responsável</Text>
+                    <TextInput
+                        style={styles.input}
+                        label="Nome"
+                        mode="outlined"
+                        value={nome}
+                        onChangeText={text => setNome(text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="CPF"
+                        mode="outlined"
+                        value={cpf}
+                        onChangeText={text => setCpf(text)}
+                        keyboardType="numeric"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="Telefone"
+                        mode="outlined"
+                        value={telefone}
+                        onChangeText={text => setTelefone(text)}
+                        keyboardType="phone-pad"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="E-mail"
+                        mode="outlined"
+                        value={email}
+                        onChangeText={text => setEmail(text)}
+                        keyboardType="email-address"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="Senha"
+                        mode="outlined"
+                        value={senha}
+                        onChangeText={text => setSenha(text)}
+                        secureTextEntry={true}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        label="Confirme sua Senha"
+                        mode="outlined"
+                        value={confSenha}
+                        onChangeText={text => setConfSenha(text)}
+                        secureTextEntry={true}
+                    />
+                    <Button 
+                        mode="contained" 
+                        onPress={handleCadastro} 
+                        style={styles.button}
+                        labelStyle={styles.buttonLabel}
+                    >
+                        Cadastrar
+                    </Button>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
