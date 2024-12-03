ab4360167c1fa033114863c0c04fdf38790669c9
Davi <davifssoares2002@gmail.com>
2024-12-02 23:01:55 -0300

    user point

--- AppTransporteEscolar/data/userPointServices.js
+++ AppTransporteEscolar/data/userPointServices.js
@@ -4,11 +4,15 @@ import { APP_URL } from "@env";
 const apiUrl = APP_URL;
 const _controller = apiUrl + '/user-point';
 
-export const getDriverByCode = async(code) => {
+export const getDriverByCode = async(code, token) => {
     const _endpoint = `/driver-school-by-code?code=${code}`;
 
     try{
-        const response  = await axios.get(_controller + _endpoint);
+        const response  = await axios.get(_controller + _endpoint, {
+            headers: {
+                Authorization: "Bearer " + token
+            }
+        });
 
         return response;
     }
